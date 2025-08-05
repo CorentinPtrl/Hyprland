546bc3fa28f87bcfdbc01b328ce1908a73277ba8
Corentin Pitrel <33176298+CorentinPtrl@users.noreply.github.com>
2025-08-05 10:04:56 +0200

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -62,7 +62,7 @@
     };
 
     xdph = {
-      url = "github:hyprwm/xdg-desktop-portal-hyprland";
+      url = "github:CorentinPtrl/xdg-desktop-portal-hyprland/feat/input-capture-impl";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.systems.follows = "systems";
       inputs.hyprland-protocols.follows = "hyprland-protocols";
